2d8efd93bbca28953a1e4370977ddcf082555b26
Sabari Raj <sabarinagsel004@gmail.com>
2023-12-11 22:12:06 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+function App() {
+  const navigate = useNavigate();
+  return (
+    <GoogleOAuthProvider clientId="1061676741686-j9flk690mn53ba8nupppk5n0h8d9t1ca.apps.googleusercontent.com">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>knkj</h1>;
+}
+
+export default Home;
